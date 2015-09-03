a476f50b100b0b497dc73d832ed786fb0a5b7a68
Shuguo Yang <yangshuguo@google.com>
2015-09-03 17:32:30 +0800

    update map key

--- app/src/main/java/moe/mzry/ilmare/MainScreenActivity.java
+++ app/src/main/java/moe/mzry/ilmare/MainScreenActivity.java
@@ -2,19 +2,26 @@ package moe.mzry.ilmare;
 
 import android.os.*;
 import android.support.v4.app.*;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-public class MainScreenActivity extends FragmentActivity {
+public class MainScreenActivity extends FragmentActivity implements OnMapReadyCallback {
 
   private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+  private TextView console;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstaState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_screen);
+
+    // Initialize the views.
+    console = (TextView) findViewById(R.id.text);
     setUpMapIfNeeded();
+
+    console.append("\n Finished!");
   }
 
   @Override
@@ -23,27 +30,14 @@ public class MainScreenActivity extends FragmentActivity {
     setUpMapIfNeeded();
   }
 
-  /**
-   * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-   * installed) and the map has not already been instantiated.. This will ensure that we only ever
-   * call {@link #setUpMap()} once when {@link #mMap} is not null.
-   * <p/>
-   * If it isn't installed {@link SupportMapFragment} (and
-   * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-   * install/update the Google Play services APK on their device.
-   * <p/>
-   * A user can return to this FragmentActivity after following the prompt and correctly
-   * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-   * have been completely destroyed during this process (it is likely that it would only be
-   * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-   * method in {@link #onResume()} to guarantee that it will be called.
-   */
   private void setUpMapIfNeeded() {
     // Do a null check to confirm that we have not already instantiated the map.
     if (mMap == null) {
       // Try to obtain the map from the SupportMapFragment.
       mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
           .getMap();
+
+      console.append("\n trying to load map!");
       // Check if we were successful in obtaining the map.
       if (mMap != null) {
         setUpMap();
@@ -59,5 +53,16 @@ public class MainScreenActivity extends FragmentActivity {
    */
   private void setUpMap() {
     mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    console.append("map set up!");
+  }
+
+  @Override
+  public void onMapReady(GoogleMap map) {
+    // Add a marker in Sydney, Australia, and move the camera.
+    LatLng sydney = new LatLng(-34, 151);
+    map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+    map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    console.append("map loaded!");
   }
 }
